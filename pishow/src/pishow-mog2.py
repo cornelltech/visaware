1356ef6be6ed838c2121abdac6ce7c5ca4241909
Doron Tal <doron@tracktunes.org>
2017-12-13 22:38:34 +0000

    showing frames per second

--- pishow/src/pishow-mog2.py
+++ pishow/src/pishow-mog2.py
@@ -3,32 +3,44 @@
 import cv2
 import urllib
 import numpy
+import time
 
 URL = 'http://128.84.84.129:8080/?action=stream'
 BUFFER_LENGTH = 1024
+FONT = cv2.FONT_HERSHEY_SIMPLEX
+FONT_SCALE = 0.5
+FONT_COLOR = 255
+FONT_LINE_TYPE = 1
 
-stream = urllib.urlopen(URL)
-fgbg = cv2.createBackgroundSubtractorMOG2()
-bytes = ''
-while(True):
-    bytes += stream.read(BUFFER_LENGTH)
-    a = bytes.find('\xff\xd8')
-    b = bytes.find('\xff\xd9')
-    if  a != -1 and b != -1:
-        jpg = bytes[a:b+2]
-        bytes = bytes[b+2:]
-        # frame = cv2.imdecode(numpy.fromstring(jpg, dtype=numpy.uint8),
-        #                      cv2.CV_LOAD_IMAGE_COLOR)
-        frame = cv2.imdecode(numpy.fromstring(jpg, dtype=numpy.uint8), 1)
 
-        # cv2.imshow('frame', frame)
+def main():
+    stream = urllib.urlopen(URL)
+    fgbg = cv2.createBackgroundSubtractorMOG2()
+    bytes = ''
+    while(True):
+        start_time = time.time()
+        bytes += stream.read(BUFFER_LENGTH)
+        a = bytes.find('\xff\xd8')
+        b = bytes.find('\xff\xd9')
+        if  a != -1 and b != -1:
+            jpg = bytes[a:b+2]
+            bytes = bytes[b+2:]
+            frame = cv2.imdecode(numpy.fromstring(jpg, dtype=numpy.uint8), 1)
 
-        fgmask = fgbg.apply(frame)
-        cv2.imshow('frame', fgmask)
+            mog2 = fgbg.apply(frame)
 
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-        #     break
-        if cv2.waitKey(1) == 27:
-            cap.release()
-            cv2.destroyAllWindows()
-            exit(0)
+            end_time = time.time()
+            fps = 1/(end_time-start_time)
+
+            cv2.putText(mog2, '%2.2f fps' % fps, (20, 460),
+                        FONT, FONT_SCALE, FONT_COLOR, FONT_LINE_TYPE)
+
+            cv2.imshow('MOG2', mog2)
+
+            if cv2.waitKey(1) == 27:
+                cv2.destroyAllWindows()
+                exit(0)
+
+    
+if __name__ == '__main__':
+    main()
--- pishow/src/pishow.py
+++ pishow/src/pishow.py
@@ -7,20 +7,32 @@ import time
 
 URL = 'http://128.84.84.129:8080/?action=stream'
 BUFFER_LENGTH = 1024
+FONT = cv2.FONT_HERSHEY_SIMPLEX
+FONT_SCALE = 0.5
+FONT_COLOR = (0, 255, 0)
+FONT_LINE_TYPE = 1
 
-stream = urllib.urlopen(URL)
-bytes = ''
-while(True):
-    bytes += stream.read(BUFFER_LENGTH)
-    a = bytes.find('\xff\xd8')
-    b = bytes.find('\xff\xd9')
-    if  a != -1 and b != -1:
-        jpg = bytes[a:b+2]
-        bytes = bytes[b+2:]
-        # img = cv2.imdecode(numpy.fromstring(jpg, dtype=numpy.uint8),
-        #                    cv2.CV_LOAD_IMAGE_COLOR)
-        img = cv2.imdecode(numpy.fromstring(jpg, dtype=numpy.uint8), 1)
-        cv2.imshow('img', img)
-        if cv2.waitKey(1) == 27:
-            cv2.destroyAllWindows()
-            exit(0)
+def main():
+    stream = urllib.urlopen(URL)
+    bytes = ''
+    while(True):
+        start_time = time.time()
+        bytes += stream.read(BUFFER_LENGTH)
+        a = bytes.find('\xff\xd8')
+        b = bytes.find('\xff\xd9')
+        if  a != -1 and b != -1:
+            jpg = bytes[a:b+2]
+            bytes = bytes[b+2:]
+            frame = cv2.imdecode(numpy.fromstring(jpg, dtype=numpy.uint8), 1)
+            end_time = time.time()
+            fps = 1/(end_time-start_time)
+            cv2.putText(frame, '%2.2f fps' % fps, (20, 460),
+                        FONT, FONT_SCALE, FONT_COLOR, FONT_LINE_TYPE)
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) == 27:
+                cv2.destroyAllWindows()
+                exit(0)
+
+    
+if __name__ == '__main__':
+    main()
