4c4f283f9ecf9416f6e2cdb60c858f93e13e2b25
Doron Tal <doron@tracktunes.org>
2017-12-15 11:01:57 -0500

    DRY version - from now on import pishow and use only it for the main_loop() to which you give a callback - the callback takes as input a frame and returns a frame

--- pishow/src/cv/pishow-mog2.py
+++ pishow/src/cv/pishow-mog2.py
@@ -4,44 +4,12 @@ import cv2
 import urllib
 import numpy
 import time
+import pishow
 
-URL = 'http://128.84.84.129:8080/?action=stream'
-BUFFER_LENGTH = 1024
-FONT_LOCATION = (550, 20)
-FONT = cv2.FONT_HERSHEY_SIMPLEX
-FONT_SCALE = 0.5
-FONT_COLOR = 255
-FONT_LINE_TYPE = 1
+FGBG = cv2.createBackgroundSubtractorMOG2()
 
-
-def main():
-    stream = urllib.urlopen(URL)
-    fgbg = cv2.createBackgroundSubtractorMOG2()
-    bytes = ''
-    while(True):
-        start_time = time.time()
-        bytes += stream.read(BUFFER_LENGTH)
-        a = bytes.find('\xff\xd8')
-        b = bytes.find('\xff\xd9')
-        if  a != -1 and b != -1:
-            jpg = bytes[a:b+2]
-            bytes = bytes[b+2:]
-            frame = cv2.imdecode(numpy.fromstring(jpg, dtype=numpy.uint8), 1)
-
-            mog2 = fgbg.apply(frame)
-
-            end_time = time.time()
-            fps = 1/(end_time-start_time)
-
-            cv2.putText(mog2, '%2.2f fps' % fps, FONT_LOCATION,
-                        FONT, FONT_SCALE, FONT_COLOR, FONT_LINE_TYPE)
-
-            cv2.imshow('MOG2', mog2)
-
-            if cv2.waitKey(1) == 27:
-                cv2.destroyAllWindows()
-                exit(0)
-
+def callback(frame):
+    return FGBG.apply(frame)
     
 if __name__ == '__main__':
-    main()
+    pishow.main_loop(callback)
--- pishow/src/cv/pishow.py
+++ pishow/src/cv/pishow.py
@@ -7,33 +7,41 @@ import time
 
 URL = 'http://128.84.84.129:8080/?action=stream'
 BUFFER_LENGTH = 1024
-FONT_LOCATION = (550, 20)
+FONT_LOCATION = (550, 460)
 FONT = cv2.FONT_HERSHEY_SIMPLEX
 FONT_SCALE = 0.5
-FONT_COLOR = (0, 255, 0)
-FONT_LINE_TYPE = 1
+FONT_COLOR = 128
+FONT_LINE_TYPE = 2
 
-def main():
+# callback is a function that takes one argument: a color image,
+# decoded from the stream without any processing. It processes that image
+# and returns a processed image.
+def main_loop(callback):
     stream = urllib.urlopen(URL)
-    bytes = ''
+    buffer = ''
+    start_time = time.time()
     while(True):
-        start_time = time.time()
-        bytes += stream.read(BUFFER_LENGTH)
-        a = bytes.find('\xff\xd8')
-        b = bytes.find('\xff\xd9')
-        if  a != -1 and b != -1:
-            jpg = bytes[a:b+2]
-            bytes = bytes[b+2:]
+        buffer += stream.read(BUFFER_LENGTH)
+        image_start = buffer.find('\xff\xd8')
+        image_end = buffer.find('\xff\xd9')
+        if  image_start != -1 and image_end != -1:
+            jpg = buffer[image_start:image_end+2]
+            buffer = buffer[image_end+2:]
             frame = cv2.imdecode(numpy.fromstring(jpg, dtype=numpy.uint8), 1)
+            processed_frame = callback(frame)
             end_time = time.time()
             fps = 1/(end_time-start_time)
-            cv2.putText(frame, '%2.2f fps' % fps, FONT_LOCATION,
+            start_time = end_time
+            cv2.putText(processed_frame, '%2.2f fps' % fps, FONT_LOCATION,
                         FONT, FONT_SCALE, FONT_COLOR, FONT_LINE_TYPE)
-            cv2.imshow('frame', frame)
+            cv2.imshow('frame', processed_frame)
             if cv2.waitKey(1) == 27:
                 cv2.destroyAllWindows()
                 exit(0)
 
+def callback(frame):
+    return frame
+
     
 if __name__ == '__main__':
-    main()
+    main_loop(callback)
