0d31f7ae151cd1d30f2fd31fbbdf549370417b98
Doron Tal <doron@tracktunes.org>
2018-01-19 16:39:44 -0500

    first test done

--- pishow/src/cv/ipvideostream.py
+++ pishow/src/cv/ipvideostream.py
@@ -1,107 +1,90 @@
+#!/usr/bin/env python
+""" ipvideostream.py """
+
 # import the necessary packages
+import urllib
+import unittest
 from threading import Thread
 import cv2
+from imutils.video import FPS
+import numpy
+
 
 # constant for buffer we hold image data in while streaming from url
 BUFFER_LENGTH = 1024
-
-START_MARKER = '\xff\xd8'
-END_MARKER = '\xff\xd9'
-
-class WebcamVideoStream:
-	def __init__(self, url):
-        self.stream = urllib.urlopen(url)
-        self.buffer = stream.read(BUFFER_LENGTH)
-        self.started = self.buffer.find(START_MARKER)
-        self.ended = self.buffer.find(END_MARKER)
-		# initialize the variable used to indicate if the thread should
-		# be stopped
-		self.stopped = False
-
-	def start(self):
-		# start the thread to read frames from the video stream
-		thread = Thread(target=self.update, args=())
-		thread.daemon = True
-		thread.start()
-		return self
-
-	def update(self):
-		# keep looping infinitely until the thread is stopped
-		while True:
-            self.buffer += stream.read(BUFFER_LENGTH)
-            self.started = tmp.find(START_MARKER)
-            self.ended = tmp.find(END_MARKER)
-            if self.started != -1 and self.ended != -1:
-                jpg = self.buffer[image_start:image_end+2]
-                # keep leftovers for next iteration
-                self.buffer = self.buffer[image_end+2:]
-                self.frame = cv2.imdecode(
-                    numpy.fromstring(jpg, dtype=numpy.uint8), 1)
-
-			# if the thread indicator variable is set, stop the thread
-			if self.stopped:
-				return
-
-			# otherwise, read the next frame from the stream
-			(self.grabbed, self.frame) = self.stream.read()
-
-	def read(self):
-		# return the frame most recently read
-		return self.frame
-
-	def stop(self):
-		# indicate that the thread should be stopped
-self.stopped = True
-
-
-
-
-from threading import Thread
-
+JPEG_START_MARKER = '\xff\xd8'
+JPEG_END_MARKER = '\xff\xd9'
+TEST_URL = 'http://128.84.84.129:8080/?action=stream'
 
 class IpVideoStream:
-	def __init__(self, url):
-		# initialize the camera and stream
-        self.url = url;
-        self.buffer = '';
+    def __init__(self, url):
         self.stream = urllib.urlopen(url)
-        self.buffer += stream.read(BUFFER_LENGTH)
+        self.buffer = self.stream.read(BUFFER_LENGTH)
+        self.buf2frame()
 
-		self.stream = self.camera.capture_continuous(self.rawCapture,
-			format="bgr", use_video_port=True)
+        # initialize the variable used to indicate if the thread should
+        # be stopped
+        self.stopped = False
+        self.frame = None
 
-		# initialize the frame and the variable used to indicate
-		# if the thread should be stopped
-		self.frame = None
-		self.stopped = False
+    def buf2frame(self):
+        """Find a jpeg image in raw stream buffer, update accordingly"""
+        iStart = self.buffer.find(JPEG_START_MARKER)
+        iEnd = self.buffer.find(JPEG_END_MARKER)
+        if iStart != -1 and iEnd != -1:
+            jpg = self.buffer[iStart:iEnd+2]
+            # grab only the leftovers, for next iteration:
+            self.buffer = self.buffer[iEnd+2:]
+            self.frame = cv2.imdecode(
+                numpy.fromstring(jpg, dtype=numpy.uint8), 1)
 
-	def start(self):
-		# start the thread to read frames from the video stream
-		t = Thread(target=self.update, args=())
-		t.daemon = True
-		t.start()
-		return self
+    def start(self):
+	# start the thread to read frames from the video stream
+	thread = Thread(target=self.update, args=())
+	thread.daemon = True
+	thread.start()
+	return self
 
-	def update(self):
-		# keep looping infinitely until the thread is stopped
-		for f in self.stream:
-			# grab the frame from the stream and clear the stream in
-			# preparation for the next frame
-			self.frame = f.array
-			self.rawCapture.truncate(0)
+    def update(self):
+	# keep looping infinitely until the thread is stopped
+	while True:
+            self.buffer += self.stream.read(BUFFER_LENGTH)
+            self.buf2frame()
 
-			# if the thread indicator variable is set, stop the thread
-			# and resource camera resources
-			if self.stopped:
-				self.stream.close()
-				self.rawCapture.close()
-				self.camera.close()
-				return
+	    # if the thread indicator variable is set, stop the thread
+	    if self.stopped:
+		return
 
-	def read(self):
-		# return the frame most recently read
-		return self.frame
+    def read(self):
+	# return the frame most recently read
+	return self.frame
 
-	def stop(self):
-		# indicate that the thread should be stopped
-self.stopped = True
+    def stop(self):
+	# indicate that the thread should be stopped
+        self.stopped = True
+
+class ModuleTests(unittest.TestCase):
+    """
+    module tests
+    """
+    @staticmethod
+    def test01():
+        """
+        can we do this?
+        """
+        videoStream = IpVideoStream(TEST_URL).start()
+        fps = FPS().start()
+
+        while fps._numFrames < 3000:
+            frame = videoStream.read()
+            if frame is not None:
+                cv2.imshow('Frame', videoStream.frame)
+                fps.update()
+
+        fps.stop()
+        print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+        print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+
+if __name__ == "__main__":
+    unittest.main()
