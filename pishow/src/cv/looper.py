2a71fb13a904887bd74a9b0abce685e467b98e46
dorontal <doron@tracktunes.org>
2018-01-24 17:49:23 -0500

    minor

--- pishow/src/cv/looper.py
+++ pishow/src/cv/looper.py
@@ -8,100 +8,77 @@ import urllib
 import time
 import numpy
 import cv2
+from fps import FPS
+from file_video_stream import FileVideoStream
+from ip_video_stream import IpVideoStream
+from webcam_video_stream import WebcamVideoStream
+from pacer import Pacer
 
+# we run everything at a constant FPS for sanity, even if can do faster
+NORMALIZED_FPS = 30
 
-# constants for fps display overlayed on the (640px x 480px) image
-FONT_LOCATION = (550, 460)
-FONT = cv2.FONT_HERSHEY_SIMPLEX
-FONT_SCALE = 0.5
-FONT_COLOR = 64
-FONT_LINE_TYPE = 2
-
-# constant for buffer we hold image data in while streaming from url
-BUFFER_LENGTH = 1024
 
 def parse_command_line(effect):
     """Process command line arguments"""
     progname = sys.argv[0]
-    usage_message = 'Usage: %s [<url>|<file-path>]\n' % progname
+    usage_message = "Usage: %s [<url>|<file-path>]\n" % progname
     n_args = len(sys.argv)-1
     if n_args == 0:
-        return lambda: loop_camera_or_file(effect, 0)
+        return lambda: loop_webcam(effect)
     elif n_args == 1:
         arg = sys.argv[1]
-        if arg == '-h' or arg == '--help':
+        if arg == "-h" or arg == "--help":
             print usage_message
             sys.exit(0)
-        elif arg[0:4] == 'http':
+        elif arg[0:4] == "http":
             return lambda: loop_url(effect, arg)
         else:
-            return lambda: loop_camera_or_file(effect, arg)
+            return lambda: loop_file(effect, arg)
     else:
         print usage_message
-        raise Exception('Only one argument allowed, you gave %d' % n_args)
+        raise Exception("Only one argument allowed, you gave %d" % n_args)
 
-def loop_camera_or_file(effect, loop_what):
-    """Loop camera or file"""
-    if loop_what == 0:
-        print 'loop_camera()'
-    else:
-        print 'loop_file(%s)' % loop_what
-    cap = cv2.VideoCapture(loop_what)
-    done = False
-    start_time = time.time()
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if ret:
-            # process frame
-            processed_frame = frame
-            if effect:
-                processed_frame = effect.apply(frame)
-            end_time = time.time()
-            show_image_w_fps(processed_frame, 1/(end_time-start_time))
-            start_time = end_time
-        elif not done:
-            done = True
-            print 'Clip ended!'
+def generic_looper(videoStream, effect):
+    """same loop code for any stream"""
+    fps = FPS().start()
+    pacer = Pacer(NORMALIZED_FPS).start()
+
+    while True:
+        frame = videoStream.read()
+    
+        if frame is not None and effect:
+            frame = effect.apply(frame)
+
+            cv2.imshow('Frame', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    # cleanup after breaking out of the while loop
-    cap.release()
+
+        fps.update()
+        pacer.update()
+
+    # clean up at the end
+    fps.stop()
+    videoStream.stop()
+
+    print "[INFO] elasped time: {:.2f}".format(fps.elapsed())
+    print "[INFO] approx. FPS: {:.2f}".format(fps.fps())
+
+def loop_webcam(effect):
+    """Loop webcam"""
+    print "loop_webcam()"
+    generic_looper(WebcamVideoStream().start(), effect)
+
+def loop_file(effect, fileName):
+    """Loop video file"""
+    print "loop_file(%s)" % fileName
+    generic_looper(FileVideoStream(fileName).start(), effect)
 
 def loop_url(effect, url):
     """Loop url"""
-    print 'loop_url(\'%s\')' % url
-    stream = urllib.urlopen(url)
-    buf = ''
-    start_time = time.time()
-    while True:
-        buf += stream.read(BUFFER_LENGTH)
-        image_start = buf.find('\xff\xd8')
-        image_end = buf.find('\xff\xd9')
-        if image_start != -1 and image_end != -1:
-            # got a complete frame, decode it
-            jpg = buf[image_start:image_end+2]
-            buf = buf[image_end+2:]
-            frame = cv2.imdecode(numpy.fromstring(jpg, dtype=numpy.uint8), 1)
-            # process frame
-            processed_frame = frame
-            if effect:
-                processed_frame = effect.apply(frame)
-            end_time = time.time()
-            show_image_w_fps(processed_frame, 1.0/(end_time-start_time))
-            start_time = end_time
+    print "loop_url(%s)" % url
+    generic_looper(IpVideoStream(url).start(), effect)
 
-        # listen to quit keystroke
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-def show_image_w_fps(frame, fps):
-    """refresh the display with contents of frame and show fps"""
-    cv2.putText(frame, '%2.2f fps' % fps, FONT_LOCATION,
-                FONT, FONT_SCALE, FONT_COLOR, FONT_LINE_TYPE)
-    cv2.imshow('frame', frame)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     (parse_command_line(None))()
     cv2.destroyAllWindows()
