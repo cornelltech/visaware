01b7ca8b90375d5eeac7d1242d8041d2fe66b752
dorontal <doron@tracktunes.org>
2018-01-24 23:33:07 -0500

    uses base class now

--- pishow/src/cv/webcam_video_stream.py
+++ pishow/src/cv/webcam_video_stream.py
@@ -3,79 +3,34 @@
 
 # import the necessary packages
 import unittest
-from threading import Thread
 import cv2
-# from imutils.video import FPS
-from fps import FPS
+import video_stream_base as base
+import looper
 
 
-class WebcamVideoStream:
-    def __init__(self, src=0):
-        # initialize the video camera stream and read the first frame
-        # from the stream
+class WebcamVideoStream(base.VideoStreamBase):
+
+    def __init__(self, src=0, fps=base.DEFAULT_FPS):
+        super(WebcamVideoStream, self).__init__(fps)
         self.stream = cv2.VideoCapture(src)
-        (self.grabbed, self.frame) = self.stream.read()
 
-        # initialize the variable used to indicate if the thread should
-        # be stopped
-        self.stopped = False
+    def loop_body(self):
+        # read the next frame from the file
+        (grabbed, frame) = self.stream.read()
 
-    def start(self):
-        # start the thread to read frames from the video stream
-        t = Thread(target=self.main_loop, args=())
-        t.daemon = True
-        t.start()
-        return self
-
-    def main_loop(self):
-        # keep looping infinitely until the thread is stopped
-        while True:
-            # if the thread indicator variable is set, stop the thread
-            if self.stopped:
-                return
-
-            # otherwise, read the next frame from the stream
-            (self.grabbed, self.frame) = self.stream.read()
-
-    def read(self):
-        # return the frame most recently read
-        return self.frame
-
-    def stop(self):
-        # indicate that the thread should be stopped
-        self.stopped = True
+        if grabbed:
+            self.frame = frame
+        else:
+            # reached the end of the video file
+            self.stop()
 
 class ModuleTests(unittest.TestCase):
     """module tests"""
     @staticmethod
     def test01():
         """can we do this?"""
-        videoStream = WebcamVideoStream().start()
-        fps = FPS().start()
-        pacer = Pacer(DESIRED_FPS).start()
-
-        while fps.nFrames < N_TEST_FRAMES:
-            frame = videoStream.read()
-            if frame is None:
-                print 'frame is none!'
-                break;
-        
-            cv2.imshow('Frame', frame)
-            fps.update()
-            pacer.update()
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-        fps.stop()
-        videoStream.stop()
-        print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-        print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-
+        looper.generic_looper(WebcamVideoStream().start())
+              
 
 if __name__ == "__main__":
-    N_TEST_FRAMES = 100000
-    DESIRED_FPS = 30
-
     unittest.main()
-        
