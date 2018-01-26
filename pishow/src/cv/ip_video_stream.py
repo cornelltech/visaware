b17e900e493e017dbb14c3b64a420c07510721d3
Doron Tal <doron@tracktunes.org>
2018-01-26 14:38:44 -0500

    another major speedup by running the string find() function only once per buffer grab

--- pishow/src/cv/ip_video_stream.py
+++ pishow/src/cv/ip_video_stream.py
@@ -20,18 +20,37 @@ class IpVideoStream(base.VideoStreamBase):
         super(IpVideoStream, self).__init__(fps)
         self.stream = urllib.urlopen(url)
         self.buffer = ''
+        self.iStart = -1
+        self.iEnd = -1
 
     def loop_body(self):
         """Find a jpeg image in raw stream buffer, update accordingly"""
         self.buffer += self.stream.read(BUFFER_LENGTH)
-        iStart = self.buffer.find(JPEG_START_MARKER)
-        iEnd = self.buffer.find(JPEG_END_MARKER)
-        if iStart != -1 and iEnd != -1:
-            jpg = self.buffer[iStart:iEnd+2]
-            # grab only the leftovers, for next iteration:
-            self.buffer = self.buffer[iEnd+2:]
-            self.frame = cv2.imdecode(
-                numpy.fromstring(jpg, dtype=numpy.uint8), 1)
+
+        # iStart = self.buffer.find(JPEG_START_MARKER)
+        # iEnd = self.buffer.find(JPEG_END_MARKER)
+        # if iStart != -1 and iEnd != -1:
+        #     jpg = self.buffer[iStart:iEnd+2]
+        #     # grab only the leftovers, for next iteration:
+        #     self.buffer = self.buffer[iEnd+2:]
+        #     self.frame = cv2.imdecode(
+        #         numpy.fromstring(jpg, dtype=numpy.uint8), 1)
+
+        if self.iStart == -1:
+            # we have not found the start marker yet, find it
+            self.iStart = self.buffer.find(JPEG_START_MARKER)
+        else:
+            # found the start marker already, only look for end marker
+            self.iEnd = self.buffer.find(JPEG_END_MARKER)
+            if self.iEnd != -1:
+                # just found end marker, have both start & end now, decode
+                jpg = self.buffer[self.iStart:self.iEnd+2]
+                # grab only the leftovers, for next iteration:
+                self.buffer = self.buffer[self.iEnd+2:]
+                self.frame = cv2.imdecode(
+                    numpy.fromstring(jpg, dtype=numpy.uint8), 1)
+                self.iStart = -1
+                self.iEnd = -1
 
 class ModuleTests(unittest.TestCase):
     """module tests"""
@@ -39,7 +58,7 @@ class ModuleTests(unittest.TestCase):
     def test01():
         """can we do this?"""
         base.VideoStreamBase.generic_looper(IpVideoStream(TEST_URL).start())
-              
+
 
 if __name__ == "__main__":
     # TEST_URL = 'http://128.84.84.129:8080/?action=stream'
