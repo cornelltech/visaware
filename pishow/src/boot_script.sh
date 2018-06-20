83bae77831e6e64eb68e27532a7c821eac49d529
Doron Tal <doron@tracktunes.org>
2018-06-20 16:31:07 -0400

    minor

--- pishow/src/boot_script.sh
+++ pishow/src/boot_script.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # This is the script that user pi runs on pishow boot. This starts everything.
@@ -5,20 +6,14 @@
 # This script is started on a Raspberry Pi board that has been set up for
 # 1) Automatic login of user pi into a graphical desktop (to set this auto-
 #    login up, use the command `sudo raspi-config`)
-# 2) Add the following line to `/home/pi/.config/lxsession/LXDE/autostart`:
+# 2) Add the following line to `/home/pi/.config/lxsession/LXDE-pi/autostart`:
 # @lxterminal --command="/home/pi/workspace/visaware/pishow/src/boot_script.sh"
-
-# The IP of the other pishow board that this board communicates with
 
 setterm -powerdown 0
 
 MY_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-echo $MY_DIR
-
 CMD="$MY_DIR/avg_frames_on_button_click.py"
-
 LOG_FILE="/home/pi/logs/boot_script`date +%F`.log"
 
-echo "boot_script.sh: starting..."
+echo "`date +%F` -- boot_script.sh: starting..." >> "$LOG_FILE"
 DISPLAY=:0 "$CMD" >> "$LOG_FILE" 2>&1
